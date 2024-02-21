c992aec72a3e383ce25609404fcc1e75337a36ae
Marcel Gatete <mgatete@bk.rw>
2024-02-21 21:49:17 +0200

    feat: form builder nav

---
+++ src/components/dashboard/SaveFormBtn.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { Icon } from "@iconify/react";
+
+const SaveFormBtn = () => {
+  return (
+    <Button className="gap-2" variant={"outline"}>
+      <Icon icon="akar-icons:save" className="w-4 h-4" />
+      Save
+    </Button>
+  );
+};
+
+export default SaveFormBtn;
